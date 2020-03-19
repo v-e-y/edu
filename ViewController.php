4f3c63e7d353b9f659a5166687fabddb297d4754
Edward V <v-e-y@outlook.com>
2020-03-19 15:26:09 +0200

    Try to create simple view controller

---
+++ ViewController.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+/**
+ * Base/Simple view class
+ */
+
+class Template
+{
+    // Folder with templates
+
+    // Method to change templates folder
+    
+    // Template to view/show. Find and return
+
+    // Prepare template and data to show
+    
+    // Return view
+}
