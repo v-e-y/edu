0b0fa06f1cb5cbec9be52942cdee6241d5735834
VEY <v.eduard.y@gmail.com>
2020-03-19 15:58:56 +0200

    add construct, class prop, 1 method

--- ViewController.php
+++ ViewController.php
@@ -6,9 +6,28 @@
 class Template
 {
     // Folder with templates
+    /**
+     * base folder with templates
+     */
+    private $templatesFolder;
+
+    function __construct(string $templatesFolder = 'template')
+    {
+        // TODO Hmmm, think it's a bullshit
+        $this->templatesFolder = $templatesFolder ?? 'template';
+    }
 
     // Method to change templates folder
-    
+    /**
+     * Change base template folder
+     * @param string $newBaseTemplateFolder
+     * @return void
+     */
+    protected function changeTemplateFolder (string $newBaseTemplateFolder):void
+    {
+        $this->templatesFolder = $newBaseTemplateFolder;
+    }
+
     // Template to view/show. Find and return
 
     // Prepare template and data to show
