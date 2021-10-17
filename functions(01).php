8c007927e217a0721b79809eb52eb20479fc5309
V-E-Y <v.eduard.y@gmail.com>
2021-10-17 23:16:14 +0300

    complate snd task

--- functions(01).php
+++ functions(01).php
@@ -11,7 +11,6 @@ echo '<pre>';
  * Создайте функцию odd(), которая принимает в качестве аргумента целое число 
  * и возвращает true если число не четное и false если четное.
  */
-
  function odd (int $integerToCheck):bool
  {
     return ($integerToCheck % 2) ? true : false;
@@ -20,3 +19,20 @@ echo '<pre>';
 echo (odd(5) ? 'true' : 'false') . '<br>';
 
 
+/**
+ * Создайте фукцию sum(), которая принимает любое количество числовых аргументов и возвращает их сумму.
+ */
+function sum (...$sumOfValues)
+{
+   $theSumOfValues = 0;
+   foreach ($sumOfValues as $value) {
+      // if the value not an integer or float numb skip him
+      if (!is_float($value) && !is_int($value)) {
+         continue;
+      }
+      $theSumOfValues += $value;
+   }
+   return $theSumOfValues;
+}
+
+echo sum(2.5, 6, 5, 4.2);
