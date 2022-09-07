906a672768dd318590ea1a03baffd94d8bd5975a
v-e-y <v.eduard.y@gmail.com>
2022-09-07 20:56:21 +0300

    ref repo, rm somefolders and files

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1);
-
-return [
-    // Show all errors
-    'errors' => require_once('./errors.php'),
-    // Root to folder with test data
-    'data' => __DIR__ . '/data',
-
-];
