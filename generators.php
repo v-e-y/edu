f07fc4097ae5a6f90796d27af3b31d8b02e79300
v-e-y <v.eduard.y@gmail.com>
2022-09-08 20:50:23 +0300

    complete generator read file task

--- generators.php
+++ generators.php
@@ -1,13 +1,13 @@
 <?php
 
 declare(strict_types=1);
+/*
+* Just practice
 
-$intArr = range(0, 15);
+$intArr = range(0, 27);
 $stringArr = ['test', 'string', 'array'];
-
 $resArr = array_merge($intArr, $stringArr);
-
-function collect(iterable $arr, callable $callback): Iterator
+function collect(iterable $arr, callable $callback): Iterator // Generator
 {
     foreach ($arr as $value) {
         if ($callback($value)) {
@@ -15,15 +15,31 @@ function collect(iterable $arr, callable $callback): Iterator
         }
     }
 }
-
 $res = collect($resArr, function($i) {
     if (getType($i) === 'integer' && $i % 2 === 0) {
         return 'item - ' . $i;
     }
 });
-
-
 foreach ($res as $value) {
     echo $value;
-    echo '<br>';
+    echo 
+*/
+
+/*
+* Reed json file with generator
+*/
+
+$file = new SplFileObject($_SERVER['DOCUMENT_ROOT'] . '/data/example.json');
+
+function freed(SplFileObject $file): Iterator
+{
+    while (!$file->eof()) {
+        yield $file->fgets();
+    }
 }
+
+foreach (freed($file) as $value) {
+    echo $value;
+}
+
+
