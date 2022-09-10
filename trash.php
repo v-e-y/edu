6b82f466b57bda3a416891e2a63508bfb6ee4731
v-e-y <v.eduard.y@gmail.com>
2022-09-10 12:31:05 +0300

    before merge

--- trash.php
+++ trash.php
@@ -1,26 +1,5 @@
-<?php declare(strict_types=1);
+<?php 
 
-// echo count([]);
+declare(strict_types=1);
 
-/*
-// test static var in function
-function staticVarAtFunc ()
-{
-    static $counter = 0;
-    return ++$counter;
-}
-*/
-
-print_r($_POST);
-
-?>
-
-
-
-<form method="post">
-	<input type="text" name="one" value="1" />
-	<input type="text" name="one" value="2" />
-	<input type="text" name="one" value="3" />
-
-	<input type="submit" value="Send" />	
-</form>
+echo phpinfo();
