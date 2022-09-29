5d4fc6db9f0dac83169ddf46ad40ca166974c580
v-e-y <v.eduard.y@gmail.com>
2022-09-29 16:14:15 +0300

    done with generators

--- index.php
+++ index.php
@@ -4,5 +4,27 @@ declare(strict_types=1);
 
 echo '<pre>';
 
+$arr = [
+    0,
+    2,
+    3,
+    3,
+    2,
+    5,
+    0,
+];
 
-require_once $_SERVER['DOCUMENT_ROOT'] . '/generators.php';
+
+
+$res = array_filter(array_count_values($arr), function($item) {
+    return $item;
+});
+var_dump($res);
+die;
+
+function findIt(array &$seq): int
+{
+    return 1;
+}
+
+var_dump(findIt($arr));
